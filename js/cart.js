e3566f94c7b9bfeba5d25da1f743b430ad52f108
Dara Prak <dprak107@gmail.com>
2022-06-05 04:31:58 -0700

    added some javascript

---
+++ js/cart.js
@@ -0,0 +1,105 @@
+console.log("cart.js is loaded");
+
+let carts = document.querySelectorAll('.buy-button');
+
+let products = [
+    {
+        name: 'Assassin\'s Creed: The Ezio Collection',
+        console: 'Playstation 4',
+        price: 26.99,
+        inCart: 0
+    },
+    {
+        name: 'Assassin\'s Creed: The Ezio Collection',
+        console: 'Xbox One',
+        price: 26.99,
+        inCart: 0
+    },
+    {
+        name: 'Batman: Arkham Knight',
+        console: 'Playstation 4',
+        price: 8.99,
+        inCart: 0
+    },
+    {
+        name: 'Batman: Arkham Knight',
+        conole: 'Xbox One',
+        price: 8.99,
+        inCart: 0
+    },
+
+]
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i])
+    })
+
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if (productNumbers) {
+        document.querySelector('.nav-item span').textContent = productNumbers;
+    }
+
+}
+
+function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.nav-item span').textContent = productNumbers + 1;
+       
+    } 
+    else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.nav-item span').textContent = 1;
+    }
+
+    setItems(product);
+
+}
+
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if (cartItems != null) {
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    }
+    else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+
+}
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    }
+    else {
+        localStorage.setItem("totalCost", product.price);
+    }
+
+}
+
+onLoadCartNumbers();
---
+++ js/products.js
@@ -0,0 +1,40 @@
+console.log("products.js is loaded");
+
+const products = [
+    {
+        id: 0,
+        name: "Assassin's Creed: The Ezio Collection - PlayStation 4",
+        price: 26.99,
+        instock: 100,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./images/games/assassinscreed.jpeg",
+      },
+      {
+        id: 1,
+        name: "Assassin's Creed: The Ezio Collection - Xbox One",
+        price: 26.99,
+        instock: 43,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "./images/games/assassinscreed.jpeg",
+      },
+      {
+        id: 2,
+        name: "Batman: Arkham Knight - PlayStation 4",
+        price: 8.99,
+        instock: 10,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "/images/games/batman.jpg",
+      },
+      {
+        id: 3,
+        name: "Batman: Arkham Knight - Xbox One",
+        price: 8.99,
+        instock: 5,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "/images/games/batman.jpg",
+      },
+    ];
